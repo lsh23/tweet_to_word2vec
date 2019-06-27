e077a9b4b17860fc082e5be3df2e8174e7c5bf76
lsh23 <shseoul14@gmail.com>
2019-06-27 21:38:36 +0900

    Edit using dict

--- make_word2vec_set.py
+++ make_word2vec_set.py
@@ -10,51 +10,55 @@ import pickle
 # with open("tweet_parser_result\\_FACup\\5_5_2012_16_00.txt",'r',encoding='utf-8') as tweets:
 #     tweet = tweets.readline()
 #     print(tweet)
-def make_word2vec_set():
-    float_size = 4
-    words = []
-    vectors = []
-    with open("GoogleNews-vectors-negative300.bin\\GoogleNews-vectors-negative300.bin",'rb') as file:
-        first_line = file.readline().decode('utf-8').strip('\n').split(' ')
-        num_words = int(first_line[0])
-        vector_size = int(first_line[1])
-        # count=0
-        for i in range(num_words):
-            word = ''
-            # count = count + 1
-            while True:
-                input_c = struct.unpack('c', file.read(1))
-                # print(input_c)
-                try:
-                    input_c = input_c[0].decode('utf-8')
-                except UnicodeDecodeError:
-                    input_c = input_c[0].decode('latin1')
-                    #유니코드처리가 안되는 것들에 대한 처리
-                if input_c != ' ':
-                    word += input_c
-                else:
-                    break
-            # print(count)
-            word = word.lower()
-            words.append(word)
 
-            vector = list(struct.unpack('300f', file.read(float_size * vector_size)))
-            vectors.append(vector)
-            # dummy = file.read(1)
-            # print("dummy : ",dummy)
+float_size = 4
+pretrained_set_dict = dict()
+count = 0;
+with open("GoogleNews-vectors-negative300.bin\\GoogleNews-vectors-negative300.bin",'rb') as file:
+    first_line = file.readline().decode('utf-8').strip('\n').split(' ')
+    num_words = int(first_line[0])
+    vector_size = int(first_line[1])
+    # count=0
+    for i in range(num_words):
+        word = ''
+        # count = count + 1
+        if(count == 1000):
+            break
+        while True:
+            input_c = struct.unpack('c', file.read(1))
+            # print(input_c)
+            try:
+                input_c = input_c[0].decode('utf-8')
+            except UnicodeDecodeError:
+                input_c = input_c[0].decode('latin1')
+                #유니코드처리가 안되는 것들에 대한 처리
+            if input_c != ' ':
+                word += input_c
+            else:
+                break
+        # print(count)
+        word = word.lower()
+        # words.append(word)
 
-        numpy_vector = np.array(vectors)
-        return words, numpy_vector
+        vector = list(struct.unpack('300f', file.read(float_size * vector_size)))
+        # vectors.append(vector)
+        pretrained_set_dict[word]=vector
 
-words, numpy_vector = make_word2vec_set()
+        count = count + 1
+        # dummy = file.read(1)
+        # print("dummy : ",dummy)
 
-with open('./word2vec/word_list.pkl', 'wb') as f:
-    pickle.dump(words, f)
-np.save('./word2vec/word2vec_vector_not_norm.npy', numpy_vector)
+# with open('./word2vec/word2vec_vectors.pkl', 'wb') as f:
+#     pickle.dump(vectors, f)
+with open('./word2vec/pretrained_set_dict.pkl', 'wb') as f:
+    pickle.dump(pretrained_set_dict, f)
+
+# print(pretrained_set_dict['small'])
 
 # with open('./word2vec/word_list.pkl', 'rb') as f:
 #     words = pickle.load(f)
 # words
-
-# numpy_vector = np.load('./word2vec/word2vec_vector_not_norm.npy')
+# numpy_vector = np.array(vectors)
+# np.save('./word2vec/word2vec_vector_not_norm.npy',numpy_vector)
 # numpy_vector
+i
---
+++ tweet_to_vector.py
@@ -0,0 +1,35 @@
+# 트위터 파일 한줄한줄씩 읽어서 토크나이징 (" ")하고 해당 토큰들이
+# wordlist 에 있으면 해당 word에 해당하는 vector를 취하고
+# 없으면 다음 토큰으로 넘어가서 그 한 트윗에 대한 vectors를 만들어서
+# 한줄로 파일에 저장하면 됨 .
+
+import numpy as np
+import pickle
+
+# with open('./word2vec/word_list.pkl', 'rb') as f:
+#     word_list = pickle.load(f)
+#
+# with open('./word2vec/word2vec_vectors.pkl', 'rb') as f:
+#     vectors = pickle.load(f)
+with open('./word2vec/pretrained_set_dict.pkl', 'rb') as f:
+    pretrained_set_dict = pickle.load(f)
+
+# print(word_list)
+# print(vectors)
+tweet_vectors = []
+
+with open('./tweet_parser_result/_FACup/5_5_2012_16_00.txt','r',encoding='utf-8') as tweets:
+    for tweet in tweets:
+        tweet = tweets.readline()
+        tweet_words = tweet.strip().split(" ");
+        for word in tweet_words:
+            tweet_vector = []
+            word = word.strip(".")
+            if(word in pretrained_set_dict):
+                tweet_vector += pretrained_set_dict[word]
+                tweet_vectors.append(tweet_vector)
+
+print(tweet_vectors)
+
+#     tweets_word2vec_np_vector = np.array(tweet_vectors)
+# np.save('./word2vec_result/_FACup/5_5_2012_16_00.txt', tweets_word2vec_np_vector)
